ace388406bec0d56075841ef48ee1d6b5d1c8af8
thomasgalliker <thomas@bluewin.ch>
2016-01-31 15:33:46 +0100

    Add WindowsPhone8 native sample app

---
+++ Samples/ObservableViewSample.WindowsPhone8/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Phone.Controls;
+
+using ObservableViewSample.Service;
+using ObservableViewSample.ViewModel;
+
+namespace ObservableViewSample.WindowsPhone8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
