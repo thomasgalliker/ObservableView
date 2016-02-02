5c38fa9f05bfdad5c57d241058ae5c9563655a18
thomasgalliker <thomas@bluewin.ch>
2016-02-02 23:51:10 +0100

    Add QueryableExtensions with Where extension method

---
+++ ObservableView/Extensions/QueryableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ObservableView.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Where<T>(this IQueryable<T> source, Expression baseExpression, ParameterExpression parameterExpression)
+        {
+            MethodCallExpression whereCallExpression = Expression.Call(
+                typeof(Queryable),
+                "Where",
+                new[] { source.ElementType },
+                source.Expression,
+                Expression.Lambda<Func<T, bool>>(baseExpression, new[] { parameterExpression }));
+
+            return source.Provider.CreateQuery<T>(whereCallExpression);
+        }
+    }
+}
---
+++ Tests/ObservableView.Tests/Extensions/QueryableExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using FluentAssertions;
+
+using ObservableView.Extensions;
+
+using Xunit;
+
+namespace ObservableView.Tests.Extensions
+{
+    public class QueryableExtensionsTests
+    {
+        [Fact]
+        public void ShouldApplyWhereExpression()
+        {
+            // Arrange
+            var parameterExpression = Expression.Parameter(typeof(Foo), "x");
+            Expression containsExpression = GetExpression<Foo>(parameterExpression, "Bar", "a");
+            var foo1 = new Foo { Bar = "aaa" };
+            var foo2 = new Foo { Bar = "bbb" };
+            IEnumerable<Foo> viewCollection = new List<Foo> { foo1, foo2 };
+            IQueryable<Foo> queryableDtos = viewCollection.AsQueryable();
+
+            // Act
+            var whereList = queryableDtos.Where(containsExpression, parameterExpression).ToList();
+
+            // Assert
+            whereList.Should().NotBeNull();
+            whereList.Should().HaveCount(1);
+            whereList.Should().Contain(foo1);
+        }
+
+        class Foo
+        {
+            public string Bar { get; set; }
+        }
+
+        static Expression GetExpression<T>(ParameterExpression parameterExpression, string propertyName, string propertyValue)
+        {
+            var propertyExp = Expression.Property(parameterExpression, propertyName);
+            MethodInfo containsMethodInfo = ReflectionHelper<string>.GetMethod<string>((source, argument) => source.Contains(argument));
+            var someValue = Expression.Constant(propertyValue, typeof(string));
+            var containsMethodExp = Expression.Call(propertyExp, containsMethodInfo, someValue);
+
+            return containsMethodExp;
+        }
+    }
+}
