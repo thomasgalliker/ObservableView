1bc886577e55c0ef8a64bd1fe91ccf88633bc493
thomasgalliker <thomas@bluewin.ch>
2016-02-06 00:16:02 +0100

    New ctor which takes StringComparison for Ordinal/IgnoreCase comparison of strings

--- ObservableView/Searching/Operators/ContainsOperator.cs
+++ ObservableView/Searching/Operators/ContainsOperator.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -11,13 +12,22 @@ namespace ObservableView.Searching.Operators
     [DebuggerDisplay("ContainsOperator")]
     public class ContainsOperator : BinaryOperator
     {
+        private readonly StringComparison stringComparison;
+
+        public ContainsOperator(StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
+        {
+            this.stringComparison = stringComparison;
+        }
+
         public override Expression Build(IExpressionBuilder expressionBuilder, Operation operation)
         {
-            BinaryOperation binaryOperation = (BinaryOperation)operation;
+            BinaryOperation binaryOperation = (BinaryOperation)operation;  // TODO : Move to BinaryOperator
 
             var leftExpression = expressionBuilder.Build(binaryOperation.LeftOperand);
-
-            MethodInfo containsMethodInfo = ReflectionHelper<string>.GetMethod<string>((source, argument) => source.Contains(argument));
+            if (leftExpression.Type != typeof(string))
+            {
+                leftExpression = leftExpression.ToStringExpression();
+            }
 
             var rightExpression = expressionBuilder.Build(binaryOperation.RightOperand);
             if (rightExpression.Type != typeof(string))
@@ -25,10 +35,15 @@ namespace ObservableView.Searching.Operators
                 rightExpression = rightExpression.ToStringExpression();
             }
 
+            MethodInfo containsMethodInfo = ReflectionHelper<string>.GetMethod<string>((source, argument) => source.Contains(argument, this.stringComparison));
+            var stringComparisonExpression = Expression.Constant(this.stringComparison);
+
             Expression containsExpression = Expression.Call(
-                leftExpression,
+                null, 
                 containsMethodInfo,
-                rightExpression);
+                leftExpression, 
+                rightExpression,
+                stringComparisonExpression);
 
             return containsExpression;
         }
