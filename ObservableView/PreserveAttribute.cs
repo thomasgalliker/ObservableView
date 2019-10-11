919417fe1d7bbece6df66ae8a677899e474a6cdb
Thomas Galliker <thomas@bluewin.ch>
2019-10-11 10:41:44 +0200

    Add preserve attribute + preserve System.Linq.Queryable from linking

---
+++ ObservableView/PreserveAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ObservableView;
+
+[assembly: Preserve(typeof(System.Linq.Queryable), AllMembers = true)]
+[assembly: Preserve(typeof(System.Linq.Enumerable), AllMembers = true)]
+
+namespace ObservableView
+{
+    [AttributeUsage(
+        AttributeTargets.Assembly
+        | AttributeTargets.Class
+        | AttributeTargets.Struct
+        | AttributeTargets.Enum
+        | AttributeTargets.Constructor
+        | AttributeTargets.Method
+        | AttributeTargets.Property
+        | AttributeTargets.Field
+        | AttributeTargets.Event
+        | AttributeTargets.Interface
+        | AttributeTargets.Delegate,
+        AllowMultiple = true)]
+    public sealed class PreserveAttribute : Attribute
+    {
+
+        public bool AllMembers;
+        public bool Conditional;
+
+        public PreserveAttribute()
+        {
+        }
+
+        public PreserveAttribute(Type type)
+        {
+        }
+    }
+}
