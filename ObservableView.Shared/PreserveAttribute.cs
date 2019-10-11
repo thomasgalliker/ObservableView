7bc8bd9c5a508001863c51369e5fd92fcc3f5183
Thomas Galliker <thomas@bluewin.ch>
2019-10-11 18:34:59 +0200

    Apply preserve

--- ObservableView.Shared/PreserveAttribute.cs
+++ ObservableView.Shared/PreserveAttribute.cs
@@ -1,29 +1,27 @@
 ﻿using System;
-using ObservableView;
 
-namespace ObservableView
+[assembly: Preserve]
+
+[AttributeUsage(
+    AttributeTargets.Assembly
+    | AttributeTargets.Class
+    | AttributeTargets.Struct
+    | AttributeTargets.Enum
+    | AttributeTargets.Constructor
+    | AttributeTargets.Method
+    | AttributeTargets.Property
+    | AttributeTargets.Field
+    | AttributeTargets.Event
+    | AttributeTargets.Interface
+    | AttributeTargets.Delegate,
+    AllowMultiple = true)]
+public sealed class PreserveAttribute : Attribute
 {
-    [AttributeUsage(
-        AttributeTargets.Assembly
-        | AttributeTargets.Class
-        | AttributeTargets.Struct
-        | AttributeTargets.Enum
-        | AttributeTargets.Constructor
-        | AttributeTargets.Method
-        | AttributeTargets.Property
-        | AttributeTargets.Field
-        | AttributeTargets.Event
-        | AttributeTargets.Interface
-        | AttributeTargets.Delegate,
-        AllowMultiple = true)]
-    public sealed class PreserveAttribute : Attribute
+
+    public bool AllMembers;
+    public bool Conditional;
+
+    public PreserveAttribute()
     {
-
-        public bool AllMembers;
-        public bool Conditional;
-
-        public PreserveAttribute()
-        {
-        }
     }
 }
--- ObservableView.iOS/Platform.cs
+++ ObservableView.iOS/Platform.cs
@@ -3,8 +3,11 @@
 //[assembly: Foundation.Preserve(typeof(System.Linq.Queryable), AllMembers = true)]
 //[assembly: Foundation.Preserve(typeof(System.Linq.Enumerable), AllMembers = true)]
 
+[assembly: Preserve]
+
 namespace ObservableView
 {
+    [Preserve(AllMembers = true)]
     public static class Platform
     {
         public static void Init()
